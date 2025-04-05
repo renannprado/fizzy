866a80e133d8ba3464fc95d0f146e6f94ebbe361
David Heinemeier Hansson <david@37signals.com>
2025-04-05 15:26:49 +0200

    Fix up the tests

--- test/controllers/bubbles/pins_controller_test.rb
+++ test/controllers/bubbles/pins_controller_test.rb
@@ -7,17 +7,25 @@ class Bubbles::PinsControllerTest < ActionDispatch::IntegrationTest
 
   test "create" do
     assert_changes -> { bubbles(:layout).pinned_by?(users(:kevin)) }, from: false, to: true do
-      post bucket_bubble_pin_url(buckets(:writebook), bubbles(:layout))
+      perform_enqueued_jobs do
+        assert_turbo_stream_broadcasts([ users(:kevin), :pins ], count: 1) do
+          post bucket_bubble_pin_path(buckets(:writebook), bubbles(:layout))
+        end
+      end
     end
 
-    assert_redirected_to bucket_bubble_pin_url(buckets(:writebook), bubbles(:layout))
+    assert_redirected_to bucket_bubble_pin_path(buckets(:writebook), bubbles(:layout))
   end
 
   test "destroy" do
     assert_changes -> { bubbles(:shipping).pinned_by?(users(:kevin)) }, from: true, to: false do
-      delete bucket_bubble_pin_url(buckets(:writebook), bubbles(:shipping))
+      perform_enqueued_jobs do
+        assert_turbo_stream_broadcasts([ users(:kevin), :pins ], count: 1) do
+          delete bucket_bubble_pin_path(buckets(:writebook), bubbles(:shipping))
+        end
+      end
     end
 
-    assert_redirected_to bucket_bubble_pin_url(buckets(:writebook), bubbles(:shipping))
+    assert_redirected_to bucket_bubble_pin_path(buckets(:writebook), bubbles(:shipping))
   end
 end
