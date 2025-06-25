b54ab90c09d6636a8bb5fdb3a08ff13e1ba8bb7e
Jorge Manrubia <jorge@37signals.com>
2025-06-25 11:46:29 +0200

    Execute raw SQL to see if this is making production crash

--- db/migrate/20250624055720_add_description_to_cards_search_index.rb
+++ db/migrate/20250624055720_add_description_to_cards_search_index.rb
@@ -1,6 +1,8 @@
 class AddDescriptionToCardsSearchIndex < ActiveRecord::Migration[8.1]
   def change
-    drop_table :cards_search_index
+    execute <<~SQL
+      DROP TABLE cards_search_index;
+    SQL
     create_virtual_table :cards_search_index, "fts5", [ "title", "description" ]
   end
 end
